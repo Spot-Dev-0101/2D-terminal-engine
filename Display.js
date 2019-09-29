7241797abdf63d99fe6d316a4837c0b92dc5b179
itzbradmc <30415619+itzbradmc@users.noreply.github.com>
2019-09-29 23:52:38 +0100

    Started polygons
    Started work on supporting different shapes. Currently it gets all pixels on the outline of the shape, apart from the last point back to the first.
    
    Need to add the ability to fill the shape
    
    Renamed the Engine file to Game

--- Display.js
+++ Display.js
@@ -10,7 +10,7 @@ class Display{
 
     Update(objects){
         this.pixels = []
-        console.clear()
+        //console.clear()
         for(var y = 0; y < this.height; y++){
             var row = []
             for(var x = 0; x < this.width; x++){
@@ -18,39 +18,65 @@ class Display{
             }
             this.pixels.push(row)
         }
-
+        //console.log("sdfsdf: ", this.pixels, this.pixels[1].length)
         //object.points.forEach(point => {
             //this.pixels[point[1]-1][point[0]-1] = this.char
         //});
         objects.forEach(object => {
-            this.pixelsInBounds(object.maxX, object.minX, object.maxY, object.minY).forEach(pixels => {
-                this.pixels[point[1]-1][point[0]-1] = this.char
-            });
+            var outLinePixels = this.pixelsOnOutline(object.points)
+            outLinePixels.forEach(pixel => {
+                this.pixels[pixel[0]][pixel[1]] = "\x1b[36m"+this.char+"\x1b[0m"
+            })
+            this.pixelsInBounds(outLinePixels)
+            //this.pixelsInBounds(object.maxX, object.minX, object.maxY, object.minY).forEach(pixel => {
+                //this.pixels[pixel[0]][pixel[1]] = this.char
+            //});
         });
         
-
+        var output = ""
         this.pixels.forEach(row => {
             var rowStr = ""
             row.forEach(pixel => {
                 rowStr += pixel
             });
-            console.log(rowStr)
+            //console.log(rowStr)
+            output += rowStr + "\n"
         });
-
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(output);
     }
 
-    pixelsInBounds(maxX, minX, maxY, minY){
+    pixelsOnOutline(points){
         var pixels = []
+        for(var i = 0; i < points.length; i++){
+            
+            var start = points[i]
+            var end = points[i+1]
 
-        for(var y = 0; y < this.height; y++){
-            for(var x = 0; x < this.width; x++){
+            if(i == points.length-1){
+                end = points[0]
+            }
 
-                if(x >= minX && x <= maxX && y >= minY && y <= maxY){
-                    this.pixels[y][x] = this.char
-                }
-
+            if(end[0] < start[0]){
+                start = points[i+1]
+                end = points[i]
+            }
+            var length = Math.sqrt(((end[0]*end[0])-(start[0]*start[0]))+((end[1]*end[1])-(start[1]*start[1])))
+            for(var z = 0; z < length; z++){
+                var t = z/length
+                var y = Math.round((1-t)*start[0]+t*end[0])
+                var x = Math.round((1-t)*start[1]+t*end[1])
+                pixels.push([x, y])
             }
         }
+        return pixels
+    }
+
+    pixelsInBounds(outline){
+        var pixels = [] 
+
+        console.log(outline)
 
         return pixels
     }
